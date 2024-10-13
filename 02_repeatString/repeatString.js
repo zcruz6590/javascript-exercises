96f6012e48c9ab367a4c06a48c478d4755cc92cf
zcruz6590 <zoenoel_cruz@Yahoo.com>
2024-10-12 21:07:21 -0700

    repeat string completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(word, num) {
+    let myStr = ''
+    
+    if (num < 0) {
+        return 'ERROR'
+    } else {
+        if (num > 0) {
+            for(let rep = 1; rep <= num; rep++) {
+                myStr = myStr + word
+            }
+        }   return myStr
+    }
+}
 // Do not edit below this line
 module.exports = repeatString;
