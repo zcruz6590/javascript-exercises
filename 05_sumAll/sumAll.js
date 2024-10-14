e2333fb2b810bfc93f2e155ffca81d6f76223894
zcruz6590 <zoenoel_cruz@Yahoo.com>
2024-10-13 20:37:09 -0700

    summed all integers in 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    let sum = 0
+    if (int1 < 0 || int2 < 0 || !Number.isInteger(int1) || !Number.isInteger(int2)) {
+        return 'ERROR'
+    } else {
+        if (int1 < int2) {
+            for(let i = int1; i <= int2; i++) {
+                sum += i
+            }
+        }
+        if (int2 < int1) {
+            for(let i = int2; i <= int1; i++) {
+                sum += i
+            }
+        } 
+    } return sum
 };
 
 // Do not edit below this line
