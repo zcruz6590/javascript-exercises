821cd7d4d01ca044b4f9e30fb0a3cee8850177bb
zcruz6590 <zoenoel_cruz@Yahoo.com>
2024-10-13 20:16:03 -0700

    removed all elements in array that are passed into ...elements in 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+    
+    //filters each item in arr such that the item is not included in ...elements
+    //basically returns all the items in the array that are not present in ...elements
+    return arr.filter(item => !elements.includes(item))
+    
 };
-
+ 
 // Do not edit below this line
 module.exports = removeFromArray;
+
